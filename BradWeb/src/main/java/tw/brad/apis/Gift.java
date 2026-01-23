ff6790c42cbfede903cd156e34ab89e16495435d
Dennyy124986 <choustar4436@gmail.com>
2026-01-23 09:24:03 +0800

    01230923
    2026

---
+++ BradWeb/src/main/java/tw/brad/apis/Gift.java
@@ -0,0 +1,43 @@
+package tw.brad.apis;
+
+public class Gift {
+	private Long id;
+	private String name, feature, addr, tel, picurl;
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getFeature() {
+		return feature;
+	}
+	public void setFeature(String feature) {
+		this.feature = feature;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public String getPicurl() {
+		return picurl;
+	}
+	public void setPicurl(String picurl) {
+		this.picurl = picurl;
+	}
+	
+}
