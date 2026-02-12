8211e764ac469fae555e9622d3ecbe8e3d8b03ad
Dennyy124986 <choustar4436@gmail.com>
2026-02-12 10:26:11 +0800

    02121025

---
+++ Java20260102/src/tw/EEIIT/tutor/Brad66.java
@@ -0,0 +1,10 @@
+package tw.EEIIT.tutor;
+
+public class Brad66 {
+
+	public static void main(String[] args) {
+		Point p = new Point(12, 34);
+		System.out.println(p.x() + ":" + p.y());
+	}
+
+}
---
+++ Java20260102/src/tw/EEIIT/tutor/Point.java
@@ -0,0 +1,11 @@
+package tw.EEIIT.tutor;
+
+/*
+1. 全參數建構子
+2. 參數就是屬性, final
+3. 屬性就是getter 名稱
+4. equals => 針對屬性
+*/
+public record Point(int x, int y) {
+	
+}
